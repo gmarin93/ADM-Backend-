477fd7f4da36b58980e933bb3972cb06e02d8949
gmarin93 <arias.fex@gmail.com>
2020-09-16 16:50:53 -0600

    Cambios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 //Habilitar express.json para interpretar las entradas de request. Header application/json
 app.use(express.json({extended:true}));
 
-const PORT=process.env.port || 4000; //Cualquier numero que no sea 3000
+const port=process.env.port || 4000; //Cualquier numero que no sea 3000
 
 app.use('/api/usuarios',require('./routes/usuarios'));
 app.use('/api/auth',require('./routes/auth'));
@@ -22,7 +22,7 @@ app.get('/',(req,res)=>{
     res.send("Hola");
 })
 
-app.listen(PORT,()=>{
+app.listen(port, '0.0.0.0',()=>{
     console.log(`El servidor estar funcionando correctamente en el puerto: ${PORT}`);
 })
 
